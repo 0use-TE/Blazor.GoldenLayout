a967b4bf53cc4c6b26cffed16d9b0b880099e344
Ouse <2216528769@qq.com>
2025-05-12 23:29:18 +0800

    das

--- src/Blazor.GoldenLayout/wwwroot/goldenlayoutinterop.js
+++ src/Blazor.GoldenLayout/wwwroot/goldenlayoutinterop.js
@@ -4,5 +4,4 @@ export function createGoldenLayout(configuration, container)
     console.log(configuration);
     console.log('End Create GoldenLayout');
     return new GoldenLayout(configuration, container);
-
 }
